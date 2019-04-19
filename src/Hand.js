f6a750852e9b7bf7061b4aabe2255736f8137e13
BoraBoraPalm <48960401+BoraBoraPalm@users.noreply.github.com>
2019-04-20 00:07:50 +0200

    Dealer place cards issue fixed
    I fixed the issue that the dealer didn't set the cards in the right way. This means if he has shown the same cards for example two times, he added a card on the table! Now fixed.

--- src/Hand.js
+++ src/Hand.js
@@ -11,8 +11,8 @@ export default class HandboxContentStart {
     this.displayCards = new DisplayCard(); //each hand displays it's cards!
     this.placeNumber = placeNumber; // On which place the player/dealer is sitting! (this is for display cards!)
     this.handNumber = handNumber; // needed for displayCard!
-    this.sameAmountOfCardsShow === false;
-    this.replacedHiddenCard === false;
+    this.sameAmountOfCardsShow = false;
+    this.replacedHiddenCard = false;
 
     this.called = false; //DEAST, DELETE
   }
@@ -37,21 +37,28 @@ export default class HandboxContentStart {
     //    --> passiert beim ersten mal, wenn diese Funktion aufgerufen wird
     //wenn SHOW
     // *) eine Karte ist verdeckt
-    alert(action);
+    //alert(action);
+    //alert(action);
+
     if (action === "SHOW") {
       //place hidden card
-      this.displayCards.displayCardDealer(this.fileAndPathAll[0], action);
-      //place othercards (without hidden card!)
-      for (let i = this.cardsAlreadyOnTable + 1; i < this.numberOfCards; ++i) {
-        this.displayCards.displayCardDealer(this.fileAndPathAll[i], action);
+      if (this.numberOfCards > this.cardsAlreadyOnTable) {
+        this.displayCards.displayCardDealer(this.fileAndPathAll[1], action);
       }
+      this.cardsAlreadyOnTable = this.numberOfCards;
+      //place othercards (without hidden card!)
     } else if (action === "SHOWALL") {
       //if first time showall called --> replaye hidden card
-      this.displayCards.displayCardDealer(this.fileAndPathAll[0], action);
+      if (this.replacedHiddenCard === false) {
+        this.displayCards.displayCardDealer(this.fileAndPathAll[0], action);
+        this.replacedHiddenCard = true;
+      }
+
       //display all cards
-      for (let i = this.cardsAlreadyOnTable + 1; i < this.numberOfCards; ++i) {
+      for (let i = this.cardsAlreadyOnTable; i < this.numberOfCards; ++i) {
         this.displayCards.displayCardDealer(this.fileAndPathAll[i], action);
       }
+      this.cardsAlreadyOnTable = this.numberOfCards;
     }
     /*
     //#b if number of cards are changed, dealer can place new card or/and he can show hidden card
